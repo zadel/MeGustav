08a3427e075092753aa849b3dfbcc9a99328999e
Adel <z.il@live.com>
2016-02-21 01:48:39 +0200

    adding notification

--- app/src/main/java/com/example/adel/megustav/MainActivity.java
+++ app/src/main/java/com/example/adel/megustav/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.adel.megustav;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*        MosesCrawl x = new MosesCrawl();
+        List<EmbraceTheWorld> et = x.getETWs();
+        for(EmbraceTheWorld e: et) {
+            getRow(e);
+        }*/
+        customnotify();
+    }
+
+    private void customnotify() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.logo_invert)
+                        .setContentTitle("חובק עולם")
+                        .setContentText("פראנצ'!!!");
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    private TableRow getRow(EmbraceTheWorld embraceTheWorld) {
+        TableRow row = new TableRow(this.getApplicationContext());
+        TextView text = new TextView(this.getApplicationContext());
+        text.setText(embraceTheWorld.getmName());
+        row.addView(text);
+        return row;
     }
 }
