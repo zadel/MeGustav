f1d55d665e20494818081c2b54f0dbf929f70e33
Adel <z.il@live.com>
2016-02-21 00:32:18 +0200

    reinitialized files

---
+++ app/src/main/java/com/example/adel/megustav/EmbraceTheWorld.java
@@ -0,0 +1,27 @@
+package com.example.adel.megustav;
+
+/**
+ * Created by adel on 21/02/16.
+ */
+public class EmbraceTheWorld {
+    private String mName;
+    private String mPhotoURL;
+    private String mDescription;
+    private Integer mPrice;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmPhotoURL() {
+        return mPhotoURL;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public Integer getmPrice() {
+        return mPrice;
+    }
+}
---
+++ app/src/main/java/com/example/adel/megustav/MosesCrawl.java
@@ -0,0 +1,12 @@
+package com.example.adel.megustav;
+
+import java.util.List;
+
+/**
+ * Created by adel on 21/02/16.
+ */
+public class MosesCrawl {
+    public List<EmbraceTheWorld> getETWs(){
+        return null;
+    }
+}
